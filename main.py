37904f5853fd0292ff009b5ac6ee1c9e2001858d
r1v3ra-1508 <dsd436938@gmail.com>
2025-09-28 17:09:05 +0300

    feat: control btns, length detection

--- main.py
+++ main.py
@@ -3,15 +3,45 @@ gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, GdkPixbuf
 from PIL import Image
 import mutagen
+from pygame import mixer
+import time
+
+is_playing = False
+
+def playSong(button, song, icon_pause, icon_play):
+    global is_playing
+    mixer.init()
+    mixer.music.load(song)
+    mixer.music.play()
+
+    if is_playing == False:
+        button.set_child(icon_pause)
+        is_playing = not is_playing
+    else: 
+        button.set_child(icon_play)
+        is_playing = not is_playing
+        stopSong(button, song)
+
+def stopSong(button, song):
+    mixer.music.load(song)
+    mixer.music.stop()
 
 def get_song_info(song):
     audio = mutagen.File(song)
     tags = audio.tags
 
+    len_min = int(audio.info.length//60)
+    len_sec = int(audio.info.length%60)
+    length = int(audio.info.length)
+
     info = {
         'title': str(tags['TIT2']),
         'artist': str(tags['TPE1']),
-        'cover': None
+        'cover': None,
+        'len_min': len_min,
+        'len_sec': len_sec,
+        'len': f"{len_min}:{len_sec}",
+        'sec_len': length
     }
 
     if song.endswith('.mp3'):
@@ -52,6 +82,33 @@ def main(app):
     cover_wrap.append(cover)
     cover_wrap.set_halign(Gtk.Align.CENTER)
 
+    position_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    position = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL)
+    position.set_range(0, 100)
+    length = Gtk.Label(label=f'{song_info['len']}')
+    position_box.append(length)
+    position_box.append(position)
+
+    controls = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=28)
+    
+    prev = Gtk.Picture.new_for_filename('data/prev.svg')
+    play = Gtk.Picture.new_for_filename('data/play.svg')
+    pause = Gtk.Picture.new_for_filename('data/pause.svg')
+    next = Gtk.Picture.new_for_filename('data/next.svg')
+    
+    prev_btn = Gtk.Button()
+    prev_btn.set_child(prev)
+    play_btn = Gtk.Button()
+    play_btn.set_child(play)
+    play_btn.connect('clicked', lambda btn: playSong(btn, song, pause, play))
+    next_btn = Gtk.Button()
+    next_btn.set_child(next)
+
+    controls.append(prev_btn)
+    controls.append(play_btn)
+    controls.append(next_btn)
+
+
     styles = """picture{ border-radius: 10px; } .info{ font-size: 20px; }"""
     css = Gtk.CssProvider()
     css.load_from_data(styles.encode())
@@ -60,6 +117,8 @@ def main(app):
 
     container.append(cover_wrap)
     container.append(info_wrap)
+    container.append(position_box)
+    container.append(controls)
 
     win.set_child(container)
     win.present()
